4c1d413b7b340ba12fb9984cc51083e316c05c3c
gihubme <n.karakin@qmul.ac.uk>
2022-05-06 11:25:48 +0200

    adding client methods for customer with tests

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.config.ConfigProps;
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Component
 public class BreweryClient {
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private final String apihost;
 
     private final RestTemplate restTemplate;
@@ -28,14 +30,29 @@ public class BreweryClient {
 
     public URI saveNewBeer(BeerDto dto) {
         return restTemplate.postForLocation(apihost + BEER_PATH_V1, dto);
-
     }
 
     public void updateBeerById(UUID id, BeerDto dto) {
         restTemplate.put(apihost + BEER_PATH_V1 + id.toString(), dto);
     }
 
-    public void deleteById(UUID id) {
+    public void deleteBeerById(UUID id) {
         restTemplate.delete(apihost + BEER_PATH_V1 + id.toString());
+    }
+
+    public CustomerDto getCustomerById(UUID id) {
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + id.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto dto) {
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, dto);
+    }
+
+    public void updateCustomerById(UUID id, CustomerDto dto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + id.toString(), dto);
+    }
+
+    public void deleteCustomerById(UUID id) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + id.toString());
     }
 }
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +22,7 @@ class BreweryClientTest {
     @Test
     void getBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
-
         assertNotNull(dto);
-
     }
 
     @Test
@@ -45,7 +44,38 @@ class BreweryClientTest {
     }
 
     @Test
-    void deleteById() {
-        client.deleteById(UUID.randomUUID());
+    void deleteBeerById() {
+        client.deleteBeerById(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+
+    }
+
+    @Test
+    void saveNewCustomer() {
+        //given
+        CustomerDto dto = CustomerDto.builder().name("Some new Customer").build();
+        URI uri = client.saveNewCustomer(dto);
+
+        assertNotNull(uri);
+        log.debug("Uri: " + uri.toString());
+    }
+
+    @Test
+    void updateCustomerById() {
+        //given
+        CustomerDto dto = CustomerDto.builder().name("Some new Customer").build();
+
+        client.updateCustomerById(UUID.randomUUID(), dto);
+    }
+
+    @Test
+    void deleteCustomerById() {
+        client.deleteCustomerById(UUID.randomUUID());
     }
 }
